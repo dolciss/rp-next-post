138c6bdbdb244b8b9d39ef7daa5027feacbd33ca
L-tan <3786294+dolciss@users.noreply.github.com>
2023-09-15 22:29:50 +0900

    フィードにrequesterDIDを渡す

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -5,7 +5,7 @@ import {
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
 import * as rpNextPost from './rp-next-post'
 
-type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
+type AlgoHandler = (ctx: AppContext, params: QueryParams, requester: string) => Promise<AlgoOutput>
 
 const algos: Record<string, AlgoHandler> = {
   [rpNextPost.shortname]: rpNextPost.handler,
